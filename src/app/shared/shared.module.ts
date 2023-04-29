120be97d14a28726309102bc7b75b085d4c6a2fd
santiagoMoreno73 <santimor2009@hotmail.com>
2023-04-28 21:59:21 -0500

    add page not found

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OperacionesComponent } from './components/operaciones/operaciones.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [OperacionesComponent],
   exports: [OperacionesComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ]
 })
 export class SharedModule { }
