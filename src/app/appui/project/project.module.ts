edbc93a72567b355dd086d7d0a176b1462833dd2
santiagoMoreno73 <santimor2009@hotmail.com>
2023-04-28 21:57:17 -0500

    add project module

---
+++ src/app/appui/project/project.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FormsModule } from '@angular/forms';
+import { ScheduleModule } from './schedule/schedule.module';
+import { OperationsModule } from './operaciones/operations.module';
+
+@NgModule({
+  imports: [CommonModule, ScheduleModule, OperationsModule],
+  declarations: [],
+  exports: [],
+})
+export class ProjectModule {}
