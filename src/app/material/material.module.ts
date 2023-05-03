0230295fed4d0cc5bbf01bcabb1bf67f9e178685
santiagoMoreno73 <santimor2009@hotmail.com>
2023-05-03 17:35:26 -0500

    add material module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 @NgModule({
-  exports: [MatExpansionModule, MatTabsModule],
+  exports: [
+    MatExpansionModule,
+    MatTabsModule,
+    MatTableModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatTooltipModule,
+  ],
 })
 export class MaterialModule {}
