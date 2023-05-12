a9edd16f015836b81e40118ff054d69eea903409
santiago.moreno@geoaitech.com <santiago.moreno@geoaitech.com>
2023-05-11 21:08:29 -0500

    add directive

---
+++ src/app/auth/directives/show-for-roles.directive.ts
@@ -0,0 +1,37 @@
+import { Directive, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Role } from '@app/models';
+import { Subscription, map, tap } from 'rxjs';
+import { AuthenticationService } from '../service/auth.service';
+
+@Directive({
+  selector: '[appShowForRoles]',
+})
+export class ShowForRolesDirective implements OnInit {
+  @Input('appShowForRoles') appShowForRoles?: Role[];
+  private sub?: Subscription;
+
+  constructor(
+    private authService: AuthenticationService,
+    private viewContainerRef: ViewContainerRef,
+    private templateRef: TemplateRef<any>
+  ) {}
+
+  ngOnInit(): void {
+    this.sub = this.authService.user$
+      .pipe(
+        map((user) =>
+          Boolean(user && this.appShowForRoles?.includes(user.role))
+        ),
+        tap((hasRole) =>
+          hasRole
+            ? this.viewContainerRef.createEmbeddedView(this.templateRef)
+            : this.viewContainerRef.clear()
+        )
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy() {
+    this.sub?.unsubscribe();
+  }
+}
