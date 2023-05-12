1cee225a8f68fdf434e2782c3646cb9a0faa7749
santiago.moreno@geoaitech.com <santiago.moreno@geoaitech.com>
2023-05-11 21:08:20 -0500

    add guard

---
+++ src/app/auth/guards/isLoggedInGuard/is-logged-in.guard.ts
@@ -0,0 +1,14 @@
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '@app/auth/service/auth.service';
+import { take, tap } from 'rxjs';
+
+export const AuthGuard = () => {
+  const authService = inject(AuthenticationService);
+  const router = inject(Router);
+
+  return authService.isAuthenticated().pipe(
+    take(1),
+    tap((isLoggedIn) => (!isLoggedIn ? router.navigate(['/login']) : true))
+  );
+};
