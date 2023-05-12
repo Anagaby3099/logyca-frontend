e9f4e025ef3c3305405a7b247cfd1b02ec56db73
santiago.moreno@geoaitech.com <santiago.moreno@geoaitech.com>
2023-05-11 21:08:09 -0500

    add home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from '@app/auth/service/auth.service';
 import { IRuta, ROUTES_CONFIG } from '@app/constantes/routes.const';
 
 @Component({
@@ -10,6 +11,12 @@ export class HomeComponent {
   showSideBar: boolean = true;
   rutas: IRuta[] = ROUTES_CONFIG;
   rutaImg: string = '/assets/descargar.jpg';
+
+  constructor(private athService: AuthenticationService) {}
+
+  logout() {
+    this.athService.logout();
+  }
 
   toogleSideBar(): void {
     this.showSideBar = !this.showSideBar;
