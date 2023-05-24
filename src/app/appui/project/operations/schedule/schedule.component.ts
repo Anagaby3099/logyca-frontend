7fac9e773b133b3cfd60e962272fdc16bc8e5e47
Ana Gabriela Vargas <avargas@softmanagement.com.co>
2023-05-24 08:57:16 -0500

    correciones Boostrap

--- src/app/appui/project/operations/schedule/schedule.component.ts
+++ src/app/appui/project/operations/schedule/schedule.component.ts
@@ -1,9 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import {
-  NgbCalendar,
-  NgbDate,
-  NgbDateParserFormatter,
-} from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit, ViewChild } from '@angular/core'; 
 import { DATA_TABLA, IDataTabla } from 'src/app/constantes/data-tabla.const';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
@@ -21,90 +16,37 @@ export class ScheduleComponent implements OnInit {
 
   page: number = 1;
   pageSize: number = 10;
-  hoveredDate: NgbDate | null = null;
-
+  
   fromDate: any;
-  toDate: NgbDate | null = null;
-
+  
   constructor(
-    private calendar: NgbCalendar,
-    public formatter: NgbDateParserFormatter
   ) {
-    this.fromDate = calendar.getToday();
-    this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
+ 
   }
-
   ngOnInit(): void {
     this.refreshData();
     this.breakpoint = window.innerWidth <= 700 ? 1 : 2;
   }
-
   ngAfterViewInit() {
     this.data.paginator = this.paginator;
   }
-
   onResize(event: any) {
     this.breakpoint = event.target.innerWidth <= 700 ? 1 : 2;
   }
-
   refreshData(): void {
     // this.data = DATA_TABLA.map((dato, i) => ({ id: i + 1, ...dato })).slice(
     //   (this.page - 1) * this.pageSize,
     //   (this.page - 1) * this.pageSize + this.pageSize,
     // );
   }
-
   updateProductsPerPage() {
     this.page = 1;
     this.refreshData();
   }
-
-  onDateSelection(date: NgbDate) {
-    if (!this.fromDate && !this.toDate) {
-      this.fromDate = date;
-    } else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
-      this.toDate = date;
-    } else {
-      this.toDate = null;
-      this.fromDate = date;
-    }
-  }
-
-  isHovered(date: NgbDate) {
-    return (
-      this.fromDate &&
-      !this.toDate &&
-      this.hoveredDate &&
-      date.after(this.fromDate) &&
-      date.before(this.hoveredDate)
-    );
-  }
-
-  isInside(date: NgbDate) {
-    return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
-  }
-
-  isRange(date: NgbDate) {
-    return (
-      date.equals(this.fromDate) ||
-      (this.toDate && date.equals(this.toDate)) ||
-      this.isInside(date) ||
-      this.isHovered(date)
-    );
-  }
-
-  validateInput(currentValue: NgbDate | null, input: string): NgbDate | null {
-    const parsed = this.formatter.parse(input);
-    return parsed && this.calendar.isValid(NgbDate.from(parsed))
-      ? NgbDate.from(parsed)
-      : currentValue;
-  }
-
   addPage(): void {
     this.page++;
     this.refreshData();
   }
-
   previousPage(): void {
     this.page--;
     this.refreshData();
