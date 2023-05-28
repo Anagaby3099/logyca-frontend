f5cb43711ebebe75ef9c4f703dffb4558f7b7ee1
santiagoMoreno73 <santimor2009@hotmail.com>
2023-05-28 01:28:12 -0500

    id 5: added responsive mobile

--- src/app/appui/project/account/account.component.ts
+++ src/app/appui/project/account/account.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { BaseComponents } from '@app/shared/basecomponents';
+import { MobileDetectService } from '@app/shared/mobiledetector';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-account',
@@ -9,14 +11,23 @@ import { BaseComponents } from '@app/shared/basecomponents';
 })
 export class AccountComponent extends BaseComponents implements OnInit {
   breakpoint!: number;
+  isMobile!: boolean;
+  isMobileSubscription!: Subscription; 
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private mobileDetector: MobileDetectService) {
     super();
     this.createForm();
   }
 
   ngOnInit(): void {
     this.breakpoint = window.innerWidth <= 700 ? 1 : 4;
+    this.isMobileSubscription = this.mobileDetector.isMobile$.subscribe(
+      (is_mobile) => {
+        this.isMobile = is_mobile; 
+        console.log("@mobile", is_mobile);
+      },
+      (error) => console.log("-- Error detecting device --", error)
+    )
   }
 
   onResize(event: any) {
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,32 +1,37 @@
-import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthenticationService } from '@app/auth/service/auth.service';
 import { IRuta, ROUTES_CONFIG } from '@app/constantes/routes.const';
-import { MediaMatcher } from '@angular/cdk/layout';
+import { MobileDetectService } from '@app/shared/mobiledetector';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnDestroy {
-  mobileQuery!: MediaQueryList;
+export class HomeComponent implements OnInit, OnDestroy {
   showSideBar: boolean = true;
   rutas: IRuta[] = ROUTES_CONFIG;
   rutaImg: string = '/assets/descargar.jpg';
+  isMobile!: boolean;
+  isMobileSubscription!: Subscription; 
 
-  private _mobileQueryListener: () => void;
-  constructor(
-    private changeDetectorRef: ChangeDetectorRef,
-    private media: MediaMatcher,
-    private athService: AuthenticationService
-  ) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery?.addListener(this._mobileQueryListener);
+  constructor(private athService: AuthenticationService, private mobileDetector: MobileDetectService) {}
+
+  ngOnInit(){
+    this.isMobileSubscription = this.mobileDetector.isMobile$.subscribe(
+      (is_mobile) => {
+        this.isMobile = is_mobile; 
+        console.log("@mobile", is_mobile);
+      },
+      (error) => console.log("-- Error detecting device --", error)
+    )
   }
 
-  ngOnDestroy(): void {
-    this.mobileQuery?.removeListener(this._mobileQueryListener);
+  ngOnDestroy(){
+    if(this.isMobileSubscription){
+      this.isMobileSubscription.unsubscribe();
+    }
   }
 
   logout() {
