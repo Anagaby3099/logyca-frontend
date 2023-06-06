3bb9b491606decf2c57d8f929dc49450cc2f2380
santiago.moreno@geoaitech.com <santiago.moreno@geoaitech.com>
2023-06-06 12:29:17 -0500

    added routing create user

--- src/app/appui/project/users/users-routing.module.ts
+++ src/app/appui/project/users/users-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersMasterComponent } from './users-master/users-master.component';
+import { UserMainComponent } from './user-main/user-main.component';
 
 const routes: Routes = [
   {
     path: '',
     component: UsersMasterComponent,
+  },
+  {
+    path: 'crear',
+    component: UserMainComponent,
   },
 ];
 
